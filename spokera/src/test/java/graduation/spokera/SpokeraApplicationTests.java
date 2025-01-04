963293281b73746a809bd5fff3cb13249e15b975
cocham <mh020805@naver.com>
2025-01-04 17:43:14 +0900

    setting

---
+++ spokera/src/test/java/graduation/spokera/SpokeraApplicationTests.java
@@ -0,0 +1,13 @@
+package graduation.spokera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpokeraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
