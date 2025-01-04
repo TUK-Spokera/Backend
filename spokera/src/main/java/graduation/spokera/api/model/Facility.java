8d95d0063fc8217a6502faa0b727c4a5bf885b81
pimac <pi@pimacui-MacBookAir.local>
2025-01-05 01:14:16 +0900

    model 변수 null 가능하게

--- spokera/src/main/java/graduation/spokera/api/model/Facility.java
+++ spokera/src/main/java/graduation/spokera/api/model/Facility.java
@@ -12,15 +12,15 @@ public class Facility {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int faciId; // 시설 ID (Primary Key)
+    private Integer faciId; // 시설 ID (Primary Key)
 
     private String addrCtpvNm; // 주소 - 시/도명
     private String faciDaddr; // 상세 주소
     private String inoutGbnNm; // 실내/실외 구분명
-    private double faciLat; // 위도
-    private double faciLot; // 경도
+    private Double faciLat; // 위도
+    private Double faciLot; // 경도
     private String faciZip; // 우편번호
-    private int faciGfa; // 연면적
+    private Integer faciGfa; // 연면적
     private String ftypeNm; // 시설 유형명
     private String addrEmdNm; // 주소 - 읍/면/동명
     private String addrCpbNm; // 주소 - 추가명
