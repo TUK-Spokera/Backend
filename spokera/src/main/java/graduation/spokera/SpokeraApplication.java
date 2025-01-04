968bf3ce472eb24af2a6e8d83b078bf44d4cff0c
cocham <mh020805@naver.com>
2025-01-04 17:43:14 +0900

    setting

---
+++ spokera/src/main/java/graduation/spokera/SpokeraApplication.java
@@ -0,0 +1,13 @@
+package graduation.spokera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpokeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpokeraApplication.class, args);
+	}
+
+}
