79aede822307ef4effa0d97a97ab40866a65071d
tukjw <yyb06001@tukorea.ac.kr>
2025-01-20 02:37:45 +0900

    채팅 기능

---
+++ spokera/src/main/java/graduation/spokera/chat/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package graduation.spokera.chat.config;
+
+import graduation.spokera.chat.handler.ChatWebSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final ChatWebSocketHandler chatWebSocketHandler;
+
+    public WebSocketConfig(ChatWebSocketHandler chatWebSocketHandler) {
+        this.chatWebSocketHandler = chatWebSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatWebSocketHandler, "/ws/chat")
+                .setAllowedOrigins("*");
+    }
+}
