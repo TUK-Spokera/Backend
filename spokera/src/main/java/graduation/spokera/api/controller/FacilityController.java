921a7686aea96f3478f2ed20a0565666cb186a81
tukjw <yyb06001@tukorea.ac.kr>
2025-02-26 21:53:11 +0900

    FacilityController 복원

--- spokera/src/main/java/graduation/spokera/api/controller/FacilityController.java
+++ spokera/src/main/java/graduation/spokera/api/controller/FacilityController.java
@@ -1,23 +1,35 @@
 package graduation.spokera.api.controller;
 
+import graduation.spokera.api.domain.facility.Facility;
+import graduation.spokera.api.domain.facility.FacilityRepository;
 import graduation.spokera.api.dto.facility.FacilityLocationResponse;
 import graduation.spokera.api.service.FacilityService;
+import graduation.spokera.api.service.MatchService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/facility")
+@RequiredArgsConstructor
 public class FacilityController {
 
     private final FacilityService facilityService;
-
-    @Autowired
-    public FacilityController(FacilityService facilityService) {
-        this.facilityService = facilityService;
-    }
+    private final MatchService matchService;
+    private final FacilityRepository facilityRepository;
 
     @GetMapping
     public FacilityLocationResponse getFacilityLocation(@RequestParam String faciNm) {
         return facilityService.getFacilityLocation(faciNm);
+    }
+
+    /**
+     * 특정 매칭방의 사용자들을 기반으로 경기장 추천
+     */
+    @GetMapping("/recommend/{matchId}")
+    public List<Facility> recommendFacilitiesForMatch(@PathVariable Long matchId) {
+        return matchService.recommendFacilitiesForMatch(matchId);
     }
 }
