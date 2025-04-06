83ebc44648c50edd112ff8c46fd1d600459a431a
tukjw <yyb06001@tukorea.ac.kr>
2025-04-06 16:48:38 +0900

    종목별 랭킹정보

--- spokera/src/main/java/graduation/spokera/api/domain/user/UserRepository.java
+++ spokera/src/main/java/graduation/spokera/api/domain/user/UserRepository.java
@@ -3,10 +3,14 @@ package graduation.spokera.api.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByKakaoId(String kakaoId);
     Optional<User> findByNickname(String nickname);
+    List<User> findAllByOrderByBadmintonRatingDesc();
+    List<User> findAllByOrderByFutsalRating();
+    List<User> findAllByOrderByPingpongRating();
 }
