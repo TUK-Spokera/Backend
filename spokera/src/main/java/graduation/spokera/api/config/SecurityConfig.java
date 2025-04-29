5b6cf236594d4d5dcc8005443febde18fc38e6ad
tukjw <yyb06001@tukorea.ac.kr>
2025-04-29 21:24:11 +0900

    403 오류 발생시(로그인 안됐을때) 로그인필요 응답메세지 추가

--- spokera/src/main/java/graduation/spokera/api/config/SecurityConfig.java
+++ spokera/src/main/java/graduation/spokera/api/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package graduation.spokera.api.config;
 
 import graduation.spokera.api.domain.user.UserRepository;
 import graduation.spokera.api.util.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -23,7 +27,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .sessionManagement( session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(unauthorizedEntryPoint()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/refresh", "/oauth/kakao/**", "/ws/**").permitAll()
                         .requestMatchers("/", "/test.html", "/js/**", "/css/**").permitAll()
@@ -34,6 +39,15 @@ public class SecurityConfig {
                 .addFilterBefore(new JwtAuthenticationFilter(jwtUtil, userRepository), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    // ✅ 익명 AuthenticationEntryPoint 구현
+    private AuthenticationEntryPoint unauthorizedEntryPoint() {
+        return (HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) -> {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN); // 또는 SC_FORBIDDEN
+            response.setContentType("application/json;charset=UTF-8");
+            response.getWriter().write("{\"message\": \"로그인이 필요합니다.\"}");
+        };
     }
 
     @Bean
